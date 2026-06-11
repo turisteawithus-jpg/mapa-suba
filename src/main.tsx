cfabc5d48f58999ec8bd332656389b248da83243
TuristeaWithUs <turisteawithus@gmail.com>
2026-06-11 09:33:28 -0500

    Update main.tsx
    feat: agregar ClerkProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { ClerkProviderWrapper } from '@/components/ClerkProvider';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
-      <App />
+      <ClerkProviderWrapper>
+        <App />
+      </ClerkProviderWrapper>
     </HashRouter>
   </StrictMode>
 );
